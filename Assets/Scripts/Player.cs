ddc5c688fe7e26bf3f1347057cca4ff9950526f9
GarfGameDev <connorgamedev@hotmail.com>
2022-01-28 11:35:55 +0000

    Implemented the pickup system that adds up score by 100 per pickup and updates the score UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,20 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private CharacterController _controller;
+    [SerializeField]
+    private UIManager _uiManager;
 
     private float _speed = 6.0f;
+    private int _playerScore;
 
     private Vector3 _direction, _velocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GameObject.Find("GUI").GetComponent<UIManager>() != null)
+        {
+            _uiManager = GameObject.Find("GUI").GetComponent<UIManager>();
+        }
     }
 
     void Update()
@@ -28,5 +34,21 @@ public class Player : MonoBehaviour
         _velocity = _direction * _speed;
         _controller.Move(_velocity * Time.deltaTime);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have collided with something");
+        if (other.gameObject.tag == "Pickup")
+        {
+           
+            Pickup pickup = other.gameObject.GetComponent<Pickup>();
+
+            _playerScore += pickup.GetPickedUp();
+            _uiManager.UpdateScoreText(_playerScore);
+            pickup.gameObject.SetActive(false);
+
+            Debug.Log("The current player score is:" + _playerScore);
+        }
     }
 }
