a0509ae68c869c0c8e4005f654ad43880f2378e2
GarfGameDev <connorgamedev@hotmail.com>
2022-02-07 12:50:20 +0000

    Created a private serialized field for the EndZone box collider script

--- Assets/Scripts/EndZone.cs
+++ Assets/Scripts/EndZone.cs
@@ -4,13 +4,15 @@ public class EndZone : MonoBehaviour
 {
     [SerializeField]
     private UIManager _uIManager;
+    [SerializeField]
+    private BoxCollider _collider;
     // Check to see if player has collected enough pickups
     // and run the GameOver() method in the UI Manager
     public void CheckForGameOver(int score)
     {
         if (score > 400)
         {
-            GetComponent<BoxCollider>().enabled = false;
+            _collider.enabled = false;
             _uIManager.GameOver();
             
         }
